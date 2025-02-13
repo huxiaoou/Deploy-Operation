52758e9457b731f9631173f46929641503f8abf5
huxiaoou <516984451@qq.com>
2025-02-13 16:48:29 +0800

    Update run_all.ps1

--- daily_run.ps1
+++ daily_run.ps1
@@ -1,7 +1,7 @@
 $sig_date = Read-Host -Prompt "Please input the sig date, format=[YYYYMMDD]"
 python main.py -d $sig_date allocated
 python main.py -d $sig_date sync
-python main.py -d $sig_date positions
-python main.py -d $sig_date trades
-python main.py -d $sig_date orders --sec opn
-python main.py -d $sig_date orders --sec cls
+python main.py -d $sig_date positions # translate signals to positions
+python main.py -d $sig_date trades # calculate trades from positions
+python main.py -d $sig_date orders --sec opn # convert trades to orders with price
+python main.py -d $sig_date orders --sec cls # convert trades to orders with price
